a90e87a63ef28001ff40a01a453f64ae19300cdf
Daniel DeGroff <robotdan@maybedont.ai>
2026-01-09 19:57:49 -0700

    re-order environment variables in runLocalDockerImage.sh

--- developer/scripts/runLocalDockerImage.sh
+++ developer/scripts/runLocalDockerImage.sh
@@ -2,9 +2,9 @@
 
 docker run \
   -e MAYBE_DONT_AI_VALIDATION_API_KEY \
-  -e MAYBE_DONT_LOGGING_LEVEL \
   -e MAYBE_DONT_CONFIG_DIR=/config \
   -e MAYBE_DONT_LOG_DIR=/logs \
+  -e MAYBE_DONT_LOGGING_LEVEL \
   -v "${HOME}/.maybe-dont:/config" \
   -v "${HOME}/.maybe-dont/logs:/logs" \
   -p 8080:8080 \
